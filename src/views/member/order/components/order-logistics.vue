80e3edf0ee0ec82954d652497471fef9ec875672
KurumiRin <1548627528@qq.com>
2022-04-15 02:19:19 +0800

    商品详情头部完成 封装步骤条steps组件

--- src/views/member/order/components/order-logistics.vue
+++ src/views/member/order/components/order-logistics.vue
@@ -53,7 +53,7 @@ export default {
 }
 </script>
 <style scoped lang="less">
-.xtx-dialog ::v-deep .wrapper {
+.xtx-dialog :deep(.wrapper) {
   width: 680px;
 }
 .order-logistics-goods {
--- src/views/member/order/detail.vue
+++ src/views/member/order/detail.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>订单详情</div>
+  <div class="order-detail-page" v-if="order">
+    <!-- 操作栏 -->
+    <DetailAction :order="order" />
+    <!-- 步骤条 -->
+    <DetailSteps :order="order" />
+    <!-- 物流栏 -->
+    <!-- 订单商品信息 -->
+  </div>
 </template>
-
 <script>
-export default {}
+import DetailAction from './components/detail-action'
+import DetailSteps from './components/detail-steps'
+import { ref } from 'vue'
+import { findOrder } from '@/api/order'
+import { useRoute } from 'vue-router'
+export default {
+  name: 'OrderDetailPage',
+  components: {
+    DetailAction,
+    DetailSteps
+  },
+  setup() {
+    const order = ref(null)
+    const route = useRoute()
+    findOrder(route.params.id).then(data => {
+      order.value = data.result
+    })
+    return { order }
+  }
+}
 </script>
-
-<style></style>
+<style scoped lang="less">
+.order-detail-page {
+  background: #fff;
+}
+</style>
